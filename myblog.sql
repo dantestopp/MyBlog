a320c8251b7a0211b011c227cfadec1f91f5332f
happyoniens <dante.stoppini@gmail.com>
2014-03-10 08:49:25 +0100

    Database

---
+++ myblog.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 10. Mrz 2014 um 08:48
+-- Server Version: 5.5.27
+-- PHP-Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `myblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `t_blogpos`
+--
+
+CREATE TABLE IF NOT EXISTS `t_blogpos` (
+  `id_blogPost` int(11) NOT NULL AUTO_INCREMENT,
+  `blogTitle` varchar(100) NOT NULL,
+  `blogText` text NOT NULL,
+  `blogAuthor` int(11) NOT NULL,
+  `blogData` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_blogPost`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Daten für Tabelle `t_blogpos`
+--
+
+INSERT INTO `t_blogpos` (`id_blogPost`, `blogTitle`, `blogText`, `blogAuthor`, `blogData`) VALUES
+(1, 'First Post', 'Hallo', 1, '2014-03-10 07:48:05');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

da99f308426dd02ed8e9b7292ce8aced37cd9819
happyoniens <dante.stoppini@gmail.com>
2014-03-17 10:06:39 +0100

    Add morrisjs

--- Code/JS/myBlog.js
+++ Code/JS/myBlog.js
@@ -89,4 +89,26 @@ function loadDashboard()
 		$("#dashboard").html(data);
 	});
 }
+$("#myfirstchart").ready(function (){
 
+  var chart = new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'myfirstchart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Value']
+});
+  function loadPostStat()
+  {
+  	chart.data = "{'year':'2012','value':'12'}";
+  }
+  loadPostStat();
+});

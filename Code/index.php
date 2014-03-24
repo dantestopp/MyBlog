b9047152ddbf254873c1148a68f1a5c26240c561
happyoniens <dante.stoppini@gmail.com>
2014-03-24 09:45:12 +0100

    Alpha

---
+++ Code/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>My Blog</title>
+	<script src="Js/jquery.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div id="#show"></div>
+</body>
+</html>

101c13c4a94caf674994fa70c8f6fae184a9e413
Venyla <chewinggumtree@gmail.com>
2014-04-04 23:54:03 +0200

    Nav fixed, File renamed

--- Code/php/createPost.php
+++ Code/php/createPost.php
@@ -1,16 +1,17 @@
 <?php
 	$mysqli = new mysqli('localhost','root','root001','myblog');
-	
+	 echo '
+	  <div class="container">';
 	if(isset($_GET['id'])){
+		//bearbeiten
 		$id = $_GET['id'];
 		
 		 $sql = 'SELECT * FROM t_blogpost, t_author WHERE id_blogPost = "'.$id.'" and id_author = blogAuthor';
 		 $result = $mysqli->query($sql);
 		 $row = $result->fetch_object();
 		 
-		  echo '
-	  <div class="container">
-		<div class="row">
+		 
+		echo '<div class="row">
 				<div class="col-lg-8">
 					<!-- the actual blog post: title/author/date/content -->
 					<h1>'.$row->blogTitle.'</h1>
@@ -23,7 +24,10 @@
 
 					<hr>
 				</div>
-            </div>
-		</div>';
+            </div>';
 	}
+	else{
+		echo '<h1>please log in first!</h1>';
+	}
+	echo '</div>';
 ?>
--- Code/php/header.php
+++ Code/php/header.php
@@ -1,26 +1,15 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Home</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <div class=" navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav">
+					<li><a class="navbar-brand" href="index.php">Home</a></li>
                     <li><a href="#about">Authors</a>
                     </li>
+					<li></li>
                 </ul>
+				<div class="navbar-right">
+					<a href="login.php"><button type="button" class="btn btn-default navbar-btn ">Sign in</button></a>
+				</div>
             </div>
-			<div class="navbar-right">
-				<button type="button" class="btn btn-default navbar-btn ">Sign in</button>
-			</div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>

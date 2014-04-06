012e80b63dc40a08c30456d6a04f564f8fcf7e74
Venyla <chewinggumtree@gmail.com>
2014-04-06 14:46:21 +0200

    new Post
    wird gespeichert, noch nicht getestet

--- Code/php/newPost.php
+++ Code/php/newPost.php
@@ -1,7 +1,9 @@
 <?php
 	$mysqli = new mysqli('localhost','root','root001','myblog');
 	
-	if()
+	if(isset($_POST['blogText']) && isset($_POST['blogTitle'])){
+		 $sql = 'INSERT INTO `myblog`.`t_blogpost` (`id_blogPost`, `blogTitle`, `blogText`, `blogAuthor`, `blogDate`) VALUES (NULL, '$_POST["blogTitle"]', '$_POST["blogText"]', '$_SESSION[["userId"]', CURRENT_TIMESTAMP)';
+	}
 	
 	 echo '
 	  <div class="container">
@@ -10,7 +12,7 @@
 				<div class="col-lg-8">
 					<!-- the actual blog post: title/author/date/content -->
 					<h1>Title</h1>
-					<input type="text" size="113" />
+					<input name="blogTitle" type="text" size="113" />
 					<hr>
 					<h1>Text</h1>
 						<textarea name="blogText" style="resize: none;" class="form-control" rows="10"></textarea>

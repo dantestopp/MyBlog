6cd8e4be7c82818ad68ed8df59a0b26ff266a119
happyoniens <dante.stoppini@gmail.com>
2014-04-06 18:31:12 +0200

    Edit authors

--- Code/php/authors.php
+++ Code/php/authors.php
@@ -1,7 +1,29 @@
 <?php
 $mysqli = new mysqli('localhost','root','','myblog');
-$result = $mysqli->query("SELECT * FROM t_author WHERE allowed = 1");
+echo '<div class="container">
+	<div class="row">
+		<div class="span5">
+            <table class="table table-striped table-condensed">
+                  <thead>
+                  <tr>
+                      <th>Username</th>
+                      <th>Posts</th>                                          
+                  </tr>
+              </thead> 
+        <tbody>';
+$sql = "SELECT t_author.*, COUNT(id_blogPost) as zahl FROM t_author,t_blogpost WHERE allowed = 1 AND blogAuthor = id_author";
+$result = $mysqli->query($sql);
 while($row = $result->fetch_object())
 {
-    echo $row->username."<br/>";
+echo '            <tr>
+                    <td><a href="index.php?section=author&id='.$row->id_author.'">'.$row->username.'</a></td>
+                    <td>'.$row->zahl.'</td>
+                    </td>                                       
+                </tr>';       
 }
+        
+       echo ' </tbody>
+            </table>
+            </div>
+	</div>
+</div>';

3fc27c26dad9ba2b27fe96b22093c0eac68bd502
Venyla <chewinggumtree@gmail.com>
2014-04-06 18:19:13 +0200

    stuff

--- Code/php/newPost.php
+++ Code/php/newPost.php
@@ -1,31 +1,22 @@
 <?php
 		$mysqli = new mysqli('localhost','root','','myblog');
 		
-	if(isset($_POST['blogText']) && isset($_POST['blogTitle'])){
+		if(isset($_POST['blogText']) && isset($_POST['blogTitle']))
+        {
 			$text = $_POST['blogText'];
 			$title = $_POST['blogTitle'];
 			$author = $_SESSION['userId'];
-			 $sql = 'INSERT INTO `myblog`.`t_blogpost` (`id_blogPost`, `blogTitle`, `blogText`, `blogAuthor`, `blogDate`) VALUES (NULL, '$title', '$text', '$user', CURRENT_TIMESTAMP)';
-			$eintrag = $db->prepare( $sql );
-	        $eintrag->bind_param( $title, $tezt, $author );
-	        $eintrag->execute();
-			// Pruefen ob der Eintrag efolgreich war
-	        if ($eintrag->affected_rows == 1)
-	        {
-	            echo 'Der neue Eintrage wurde hinzugef&uuml;gt.';
-	        }
-	        else
-	        {
-	            echo 'Der Eintrag konnte nicht hinzugef&uuml;gt werden.';
-	        }
-		}
+			
+            $mysqli->query("INSERT INTO t_blogPost(id_blogPost, blogTitle, blogText, blogAuthor, blogDate) VALUES (NULL, '$title', '$text', '$user', CURRENT_TIMESTAMP)");
+            echo 'worked'; 
+        }
 		
+	
 		 echo '
 		  <div class="container">
 			<div class="row">
 					<form action="#" method="POST">
 					<div class="col-lg-8">
-						<!-- the actual blog post: title/author/date/content -->
 						<h1>Title</h1>
 						<input name="blogTitle" type="text" size="113" />
 						<hr>
